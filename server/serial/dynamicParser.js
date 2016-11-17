b736f74c49f03969c5c7184eca3409a991a94ca0
klanmiko <eternity01033@gmail.com>
2016-11-16 21:54:07 -0800

    remove console logs, added end condition

--- server/serial/dynamicParser.js
+++ server/serial/dynamicParser.js
@@ -137,19 +137,20 @@ class parseStream extends stream.Transform{ //ES6 Javascript is now just Java, a
                     return self.beginParsing(out,data,this.specification[i]);
                 }
             }
+            throw new Error("Can not found");
         }
-        if(!this.specification){
+        else if(!this.specification){
             console.log("not loaded yet");
-        }
-        return Descriptor.model.findOne({CAN_Id:data[0]}).exec().then(function(doc){
+            return Descriptor.model.findOne({CAN_Id:data[0]}).exec().then(function(doc){
         //TODO run validation
-            if(self.specification){
-                self.specification.push(doc);
-            }
-            return self.beginParsing(out,data,doc);
-        }).catch(function(){
-            throw new Error("something went horribly wrong");
-        });
+                if(self.specification&&doc){
+                    self.specification.push(doc);
+                }
+                return self.beginParsing(out,data,doc);
+            }).catch(function(){
+                throw new Error("something went horribly wrong");
+            });
+        }
     }
     parse(data){
         if(data&&data.length>0){
@@ -167,7 +168,15 @@ class parseStream extends stream.Transform{ //ES6 Javascript is now just Java, a
                 }
                 else array = data;
                 //console.log(array); 
+<<<<<<< HEAD
                 deferred.resolve(Q.fcall(this.chooseParser.bind(this),array));
+=======
+                Q.fcall(this.chooseParser.bind(this),array).then(function(value){
+                    deferred.resolve(value);
+                }).catch(function(error){
+                    deferred.reject(error);
+                });
+>>>>>>> remove console logs, added end condition
             }.bind(this));
             return deferred.promise;
         }
